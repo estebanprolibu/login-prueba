7ecf60ae6b83a3bfb12f9b009fe10c361a1bf0c7
Esteban Benjumea <benjumeac12@gmail.com>
2023-10-18 16:35:24 -0500

    Prueba

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main>
       <h1>Login</h1>
 
-      <a href="exp://192.168.10.43:8081?token=YOUR_JWT_TOKEN" target="_blank">
+      <a href="virtualcapture://?token=YOUR_JWT_TOKEN" target="_blank">
         Abrir en la aplicación
       </a>
     </main>
