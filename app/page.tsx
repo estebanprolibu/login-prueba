d9185477b928d23b6f2a9f4254dc18bc1ce975dc
Esteban Benjumea <benjumeac12@gmail.com>
2023-10-18 16:19:08 -0500

    Prueba

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
     <main>
       <h1>Login</h1>
 
-      <a
-        href="exp://192.168.10.43:8081/login?token=YOUR_JWT_TOKEN"
-        target="_blank"
-      >
+      <a href="exp://192.168.10.43:8081?token=YOUR_JWT_TOKEN" target="_blank">
         Abrir en la aplicación
       </a>
     </main>
